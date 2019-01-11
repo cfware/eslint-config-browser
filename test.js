e6f4f1975304e0e79b121b7c97df1b209b752d90
Corey Farrell <git@cfware.com>
2019-01-11 16:31:50 -0500

    fix: Add legacy.js to package.

--- test.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-
-import config from '.';
-
-test('package exports non-empty object', t => {
-	t.is(typeof config, 'object');
-	t.not(config, null);
-	t.not(Object.keys(config).length, 0);
-});
