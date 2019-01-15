9fa2e42f0e4006dd9d453636cc492dbfa674874a
Corey Farrell <git@cfware.com>
2019-01-15 11:19:32 -0500

    fix: Update legacy rules to block incompatible stuff from xo 0.24.
    * ParentNode.append is not compatible with any version of IE.
    * Element.querySelector requires IE 9

--- legacy.js
+++ legacy.js
@@ -13,6 +13,8 @@ module.exports = {
 		'node/no-unsupported-features/es-syntax': ['error', {version: '0'}],
 		'node/no-unsupported-features/es-builtins': ['error', {version: '0'}],
 		'node/no-unsupported-features/node-builtins': ['error', {version: '0'}],
-		'unicorn/prefer-add-event-listener': 'off'
+		'unicorn/prefer-add-event-listener': 'off',
+		'unicorn/prefer-node-append': 'off',
+		'unicorn/prefer-query-selector': 'off'
 	}
 };
